529fcecec382fa57c4f74553c225ee95b447d5ab
taoufiq naciri <taoufiqnaciri234@gmail.com>
2024-09-12 00:21:05 +0100

    create database composable

---
+++ composables/useDatabse.ts
@@ -0,0 +1,24 @@
+import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+let sql: postgres.Sql | null = null;
+let db: PostgresJsDatabase | null = null;
+
+function useDatabase() {
+  const config = useRuntimeConfig();
+
+  if (!config.connectionString) throw new Error('Missing db url in runtime config');
+
+  if (db && sql) return { db, sql };
+
+  try {
+    sql = postgres(config.connectionString as string);
+    db = drizzle(sql);
+  } catch (error) {
+    throw new Error(`Failed to connect to database: ${error}`);
+  }
+
+  return { db, sql };
+};
+
+export default useDatabase;
