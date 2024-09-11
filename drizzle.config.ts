f9858121c4f4e206f0f88fe5d89a542cf5438a72
taoufiq naciri <taoufiqnaciri234@gmail.com>
2024-09-12 00:20:08 +0100

    create drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './server/db/schema/*',
+  out: './server/db/migrations',
+  dialect: 'postgresql',
+  introspect: {
+    casing: 'preserve',
+  },
+  dbCredentials: {
+    url: process.env.NUXT_DATABASE_URL!,
+  },
+});
