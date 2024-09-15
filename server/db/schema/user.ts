8b566afafa03b7205c7f2e2962b0541770b33855
taoufiq naciri <taoufiqnaciri234@gmail.com>
2024-09-15 22:02:55 +0100

    update user schema

--- server/db/schema/user.ts
+++ server/db/schema/user.ts
@@ -1,9 +1,11 @@
-import { pgTable, text } from 'drizzle-orm/pg-core';
+import { bigint, pgTable, text } from 'drizzle-orm/pg-core';
 
 const user = pgTable('user', {
   id: text('id').primaryKey(),
   email: text('email').notNull(),
-  password: text('password').notNull(),
+  username: text('username').notNull(),
+  profile_picture: text('profile_picture').notNull(),
+  facebook_id: bigint('facebook_id', { mode: 'number' }).notNull().unique(),
 });
 
 export type User = typeof user.$inferSelect;
